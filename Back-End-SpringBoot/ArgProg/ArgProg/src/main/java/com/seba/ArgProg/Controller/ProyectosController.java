57cc98a66c72a896f995950896524654419d339f
SebaS <94406469+MateCosido@users.noreply.github.com>
2022-07-25 18:47:02 -0300

    Update ProyectosController.java

--- Back-End-SpringBoot/ArgProg/ArgProg/src/main/java/com/seba/ArgProg/Controller/ProyectosController.java
+++ Back-End-SpringBoot/ArgProg/ArgProg/src/main/java/com/seba/ArgProg/Controller/ProyectosController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/Proyectos")
-@CrossOrigin(origins = {"https://appargentinaprograma.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://appargentinaprograma.web.app","http://localhost:4200","https://sebaargprog-2662d.web.app/"})
 public class ProyectosController {
 
     private final ProyectosService proyectosService;
