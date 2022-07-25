bd2851d885adb5e391cfad4f91684cbc5e080616
SebaS <94406469+MateCosido@users.noreply.github.com>
2022-07-25 18:46:44 -0300

    Update ExperienciaController.java

--- Back-End-SpringBoot/ArgProg/ArgProg/src/main/java/com/seba/ArgProg/Controller/ExperienciaController.java
+++ Back-End-SpringBoot/ArgProg/ArgProg/src/main/java/com/seba/ArgProg/Controller/ExperienciaController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/experiencia")
-@CrossOrigin(origins = {"https://appargentinaprograma.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://appargentinaprograma.web.app","http://localhost:4200","https://sebaargprog-2662d.web.app/"})
 public class ExperienciaController {
     
     private final ExperienciaService experienciaService;
